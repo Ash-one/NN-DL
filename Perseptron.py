0cc8227c300f217ee4b10f91207552455f446457
Ash-one <1047141957@qq.com>
2023-03-08 11:16:50 +0800

    fix:bias逻辑错误

--- Perseptron.py
+++ Perseptron.py
@@ -12,46 +12,48 @@ class Perceptron:
     def __init__(self,dim,lr=0.1,epoch=10,activate_func=sign_func):
         self.dim = dim
         self.weight = np.random.rand(dim)
-        self.bias = np.random.rand(dim)
+        self.bias = np.random.rand(1)
         self.lr = lr
         self.epoch = epoch
         self.activate_func = activate_func
-        
+        self.last_loss = None
+    
+    def __str__(self):
+        return f'weight:{self.weight} bias:{self.bias} loss:{self.last_loss}'
+    
     def loss(self,X,Y):
         # 损失函数
-        loss = 0
+        l = 0
         for x,y in zip(X,Y):
             u = np.dot(self.weight,x) + self.bias
-            u = sum(u)
             y_pred = self.activate_func(u)
             if y_pred != y:
-                loss += 1
-        return loss
+                l += 1
+        self.last_loss = l
+        return l
 
     def train_one_step(self,x,y):
         # 训练单步
+        # 梯度下降法调整权重
         u = np.dot(self.weight,x) + self.bias
-        u = sum(u)
         y_pred = self.activate_func(u)
         if y_pred != y:
             self.weight += np.dot(self.lr * y,x)
             self.bias += self.lr * y
+        return self.weight,self.bias
 
     def train(self,X,Y):
         # 训练
         for i in range(self.epoch):
             for x,y in zip(X,Y):
                 self.train_one_step(x,y)
-            
-            print('epoch:',i,'weight:',self.weight,'bias:',self.bias)
-            print('loss:',self.loss(X,Y))
+                self.loss(X,Y)
+            # print('epoch:',i,'weight:',self.weight,'bias:',self.bias,'loss:',self.loss(X,Y)
 
     def predict(self,x):
         # 预测
         u = np.dot(self.weight,x) + self.bias
-        u = sum(u)
         return self.activate_func(u)
-
 
 
 if __name__ == '__main__':
@@ -77,6 +79,3 @@ if __name__ == '__main__':
     print('异或问题：')
     for x in X:
         print('XOR',x,p_xor.predict(x))
-
-    
-
